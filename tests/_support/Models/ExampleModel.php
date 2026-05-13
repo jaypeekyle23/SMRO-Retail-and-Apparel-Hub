38bc40ecda16de84d7be9ea8014a08db72ee465b
imcarlrc <imcarl.rc@gmail.com>
2026-05-14 00:33:44 +0800

    Update ExampleModel.php

--- tests/_support/Models/ExampleModel.php
+++ tests/_support/Models/ExampleModel.php
@@ -4,7 +4,7 @@ namespace Tests\Support\Models;
 
 use CodeIgniter\Model;
 
-class ExampleModel extends Model
+class ExampleModel extends Model 
 {
     protected $table          = 'factories';
     protected $primaryKey     = 'id';
